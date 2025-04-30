4de6acc4a9959cd1abb76c8225cd0f59140c4c91
ABAROA26 <diegoemiln_23@alu.uabcs.mx>
2025-04-30 12:23:12 -0700

    "Registro estructura MVC"

--- MVC/src/views/AuthView.java
+++ MVC/src/views/AuthView.java
@@ -237,6 +237,214 @@ public void  login() {
 }
 
 public void registro() {
+
+	JFrame ventana = new JFrame();
+	
+	ventana.setVisible(true);
+	ventana.setSize(1000, 1000);
+	ventana.setLocationRelativeTo(null);
+
+	ventana.setTitle("Login");
+
+	ventana.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+	ventana.setMaximumSize(new Dimension(800, 800));
+	ventana.setMinimumSize(new Dimension(300, 300));
+
+	ventana.setResizable(true);
+
+	
+	
+	
+	
+	
+	
+	JPanel regis = new JPanel();
+
+	regis.setBackground(Color.PINK);
+	regis.setOpaque(true);
+	regis.setSize(1000, 1000);
+	regis.setLocation(0, 0);
+    regis.setBorder(BorderFactory.createLineBorder(Color.CYAN, 10));
+
+	regis.setLayout(null); // Poder colocar las cosas donde nosotros queramos
+
+	JLabel titulo = new JLabel("Registrarse");
+	titulo.setSize(200, 50);
+	titulo.setOpaque(true);
+	titulo.setBackground(Color.CYAN);
+	titulo.setLocation(150, 30);
+	titulo.setHorizontalAlignment(JLabel.CENTER);
+	titulo.setFont(new Font("Baskerville Old Face", Font.BOLD, 30));
+	regis.add(titulo);
+
+	JLabel correo = new JLabel("Ingrese su nombre completo");
+	correo.setSize(300, 30);
+	correo.setLocation(40, 100);
+	correo.setFont(new Font("Baskerville Old Face", Font.BOLD, 20));
+	correo.setBorder(BorderFactory.createLineBorder(Color.CYAN, 3));
+
+	regis.add(correo);
+
+	JTextField escribeNombre = new JTextField();
+	escribeNombre.setSize(350, 30);
+	escribeNombre.setLocation(40, 135);
+	escribeNombre.setFont(new Font("Baskerville Old Face", Font.BOLD, 20));
+	regis.add(escribeNombre);
+
+	JLabel contraseña = new JLabel("Descripcion Personal");
+	contraseña.setSize(200, 30);
+	contraseña.setLocation(40, 190);
+	contraseña.setFont(new Font("Baskerville Old Face", Font.BOLD, 20));
+	contraseña.setBorder(BorderFactory.createLineBorder(Color.CYAN, 3));
+
+	regis.add(contraseña);
+
+	JTextArea descripcion = new JTextArea();
+	descripcion.setSize(350, 100);
+	descripcion.setLocation(40, 230);
+	descripcion.setFont(new Font("Baskerville Old Face", Font.BOLD, 20));
+	regis.add(descripcion);
+
+	JLabel preferencias = new JLabel("Preferencias");
+	preferencias.setSize(200, 30);
+	preferencias.setLocation(40, 340);
+	preferencias.setFont(new Font("Baskerville Old Face", Font.BOLD, 20));
+	preferencias.setBorder(BorderFactory.createLineBorder(Color.CYAN, 3));
+
+	regis.add(preferencias);
+
+	JCheckBox prefe = new JCheckBox("Frio", false);
+	prefe.setSize(60, 30);
+	prefe.setLocation(40, 375);
+	prefe.setOpaque(false);
+	prefe.setFont(new Font("Baskerville Old Face", Font.BOLD, 15));
+	regis.add(prefe);
+
+	JCheckBox prefe1 = new JCheckBox("Calor", false);
+	prefe1.setSize(70, 30);
+	prefe1.setLocation(120, 375);
+	prefe1.setOpaque(false);
+
+	prefe1.setFont(new Font("Baskerville Old Face", Font.BOLD, 15));
+	// prefe.setBorder(BorderFactory.createLineBorder(Color.BLACK, 2));
+	regis.add(prefe1);
+
+	JCheckBox prefe2 = new JCheckBox("Neutro", false);
+	prefe2.setSize(80, 30);
+	prefe2.setLocation(200, 375);
+	prefe2.setOpaque(false);
+
+	prefe2.setFont(new Font("Baskerville Old Face", Font.BOLD, 15));
+	// prefe.setBorder(BorderFactory.createLineBorder(Color.BLACK, 2));
+	regis.add(prefe2);
+
+	JLabel colonia = new JLabel("Elige la colonia donde vives");
+	colonia.setSize(300, 30);
+	colonia.setLocation(40, 420);
+	colonia.setFont(new Font("Baskerville Old Face", Font.BOLD, 20));
+	colonia.setBorder(BorderFactory.createLineBorder(Color.CYAN, 3));
+
+	regis.add(colonia);
+
+	String dataset[] = { " ", "Camino Real", "Centro", "Chametla", "Centenario", "Progreso" };
+
+	JComboBox colonias = new JComboBox(dataset);
+	colonias.setSize(120, 30);
+	colonias.setLocation(40, 460);
+	colonias.setFont(new Font("Baskerville Old Face", Font.BOLD, 15));
+	regis.add(colonias);
+
+	JLabel terms = new JLabel("Terminos y Condiciones");
+	terms.setSize(300, 30);
+	terms.setLocation(40, 500);
+	terms.setFont(new Font("Baskerville Old Face", Font.BOLD, 20));
+	terms.setBorder(BorderFactory.createLineBorder(Color.CYAN, 3));
+
+	regis.add(terms);
+
+	JRadioButton acepto = new JRadioButton("Acepto los términos", true);
+	acepto.setSize(140, 20);
+	acepto.setLocation(40, 540);
+	acepto.setOpaque(false);
+
+	acepto.setFont(new Font("Baskerville Old Face", Font.BOLD, 13));
+	regis.add(acepto);
+
+	JRadioButton NoAcepto = new JRadioButton("No acepto los términos", false);
+	NoAcepto.setSize(150, 20);
+	NoAcepto.setLocation(200, 540);
+	NoAcepto.setOpaque(false);
+
+	NoAcepto.setFont(new Font("Baskerville Old Face", Font.BOLD, 13));
+	regis.add(NoAcepto);
+
+	ButtonGroup opcion = new ButtonGroup();
+	opcion.add(NoAcepto);
+	opcion.add(acepto);
+
+	JButton acceder = new JButton();
+	acceder.setText("Crear Cuenta");
+	acceder.setLocation(150, 590);
+	acceder.setSize(200, 50);
+	acceder.setOpaque(true);
+	acceder.setBackground(Color.CYAN);
+	acceder.setFont(new Font("Baskerville Old Face", Font.BOLD, 25));
+
+	acceder.addActionListener(new ActionListener() {
+
+		@Override
+		public void actionPerformed(ActionEvent e) {
+
+			if (escribeNombre.getText().equals("")) {
+				escribeNombre.setBorder(BorderFactory.createLineBorder(Color.RED, 3));
+			} else {
+				escribeNombre.setBorder(BorderFactory.createLineBorder(Color.green, 3));
+
+			}
+			if (descripcion.getText().equals("")) {
+				descripcion.setBorder(BorderFactory.createLineBorder(Color.RED, 3));
+
+			} else {
+				descripcion.setBorder(BorderFactory.createLineBorder(Color.green, 3));
+
+			}
+
+		}
+	});
+
+	regis.add(acceder);
+
+	JButton ir_login = new JButton();
+	ir_login.setText("Ir al login");
+	ir_login.setLocation(150, 650);
+	ir_login.setSize(200, 50);
+	ir_login.setOpaque(true);
+	ir_login.setBackground(Color.CYAN);
+	ir_login.setFont(new Font("Baskerville Old Face", Font.BOLD, 25));
+
+	ir_login.addActionListener(new ActionListener() {
+
+		@Override
+		public void actionPerformed(ActionEvent e) {
+
+			//router("login");
+			ventana.dispose();
+			AuthView.this.login();
+		}
+
+	});
+	
+	
+
+	regis.add(ir_login);
+	
+
+	ventana.add(regis);
+	ventana.repaint();
+	ventana.revalidate();
+	ventana.setVisible(true);
+	
 	
 }
 
